d19fe92a1707d2c239d14704d3bc067954322feb
Anthamorious <116971781+Anthamorious@users.noreply.github.com>
2024-08-09 13:11:46 -0500

    Update Vol_App_backup.sql

--- old_vol_app/Vol_App_Database_Update/Vol_App_backup.sql
+++ old_vol_app/Vol_App_Database_Update/Vol_App_backup.sql
@@ -76,14 +76,15 @@ INSERT INTO `Availability` VALUES (21,10005,'Monday','06:00:00','08:00:00'),(22,
 UNLOCK TABLES;
 
 --
--- Table structure for table `Event_details`
+-- Table structure for table `EventDetails`
 --
 
-DROP TABLE IF EXISTS `Event_details`;
+DROP TABLE IF EXISTS `EventDetails`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Event_details` (
+CREATE TABLE `EventDetails` (
   `eventId` int NOT NULL AUTO_INCREMENT,
+  `creatorId` int DEFAULT NULL,
   `eventName` varchar(100) DEFAULT NULL,
   `eventDescription` text,
   `orgName` varchar(100) DEFAULT NULL,
@@ -93,24 +94,32 @@ CREATE TABLE `Event_details` (
   `eventEndDate` date DEFAULT NULL,
   `eventEndTime` time DEFAULT NULL,
   `duration` float DEFAULT NULL,
-  `creatorId` int DEFAULT NULL,
   `urgency` varchar(100) DEFAULT NULL,
   `spots` int DEFAULT NULL,
   `reqSkills` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`eventId`),
   KEY `creatorId` (`creatorId`),
-  CONSTRAINT `event_details_ibfk_1` FOREIGN KEY (`creatorId`) REFERENCES `Admins` (`userId`)
+  CONSTRAINT `eventdetails_ibfk_1` FOREIGN KEY (`creatorId`) REFERENCES `Admins` (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Event_details`
+-- Dumping data for table `EventDetails`
 --
 
-LOCK TABLES `Event_details` WRITE;
-/*!40000 ALTER TABLE `Event_details` DISABLE KEYS */;
-INSERT INTO `Event_details` VALUES (11,'Community Cleanup','A neighborhood cleanup event to help keep our community clean.','Project Cure','Central Park','2024-08-01','06:00:00','2024-08-01','08:00:00',2,10000,'Medium',15,''),(12,'Book Drive','Collect and distribute books to local schools.','Books for Kids','Local Library','2024-08-02','07:00:00','2024-08-01','09:00:00',1.5,10002,'High',10,''),(13,'Pet Adoption Fair','Event to help find homes for pets.','Houston Humane Society','Pet Shelter','2024-08-03','08:00:00','2024-08-01','10:00:00',3,10003,'Low',20,''),(14,'Blood Donation Camp','Help save lives by donating blood.','GiveBlood','Community Center','2024-08-04','09:00:00','2024-08-01','11:00:00',2.5,10004,'Medium',12,''),(25,'Community Garden Planting','Help plant new flowers and vegetables in the community garden.','Helping Hands of Houston','Community Garden','2024-08-05','07:00:00','2024-08-01','09:00:00',2,10000,'Medium',8,''),(27,'Community Garden Planting','Help plant new flowers and vegetables in the community garden.','Helping Hands of Houston','Community Garden','2024-08-05','07:00:00','2024-08-01','09:00:00',2,10000,'Medium',8,'');
-/*!40000 ALTER TABLE `Event_details` ENABLE KEYS */;
+LOCK TABLES `EventDetails` WRITE;
+/*!40000 ALTER TABLE `EventDetails` DISABLE KEYS */;
+INSERT INTO `EventDetails` VALUES (1,1,'Pet Adoption Drive','Join us for a pet adoption event to find forever homes for cats and dogs.','Houston Humane Society','1234 Animal St, Houston, TX','2024-08-15','2024-08-15','09:00:00','13:00:00',4,'Medium',15,'Arts'),
+(2,2,'Blood Donation Camp','Donate blood and save lives. Open to all eligible donors.','American Red Cross','5678 Medical Blvd, Houston, TX','2024-08-20','2024-08-20','10:00:00','14:00:00',4,'High',20,'Administrative & Clerical'),
+(3,3,'Book Drive for Schools','Collecting books for underprivileged schools in the area.','Books-For-All','9101 Book Rd, Houston, TX','2024-08-25','2024-08-25','08:00:00','12:00:00',4,'Medium',10,'Engineering'),
+(4,4,'Community Cleanup','Help clean up local parks and streets. Bring your own gloves!','Houston Cleaner-Uppers','7890 Park Ave, Houston, TX','2024-09-05','2024-09-05','07:00:00','11:00:00',4,'Low',20,'Finance'),
+(5,5,'Food Distribution Event','Distributing food to those in need in our community.','Houston Food Bank','3456 Food St, Houston, TX','2024-09-10','2024-09-10','14:00:00','18:00:00',4,'High',18,'Food Service & Events'),
+(6,16,'Tech for Good Hackathon','A hackathon focused on solving community issues through technology.','Tech Innovators Inc.','1234 Tech Lane, Houston, TX','2024-08-18','2024-08-18','09:00:00','17:00:00',8,'High',20,'Interactive & Web Development'),
+(7,17,'Sustainability Workshop','Learn about sustainable practices and how to implement them.','Green Future Foundation','5678 Green St, Houston, TX','2024-09-12','2024-09-12','10:00:00','13:00:00',3,'Medium',12,'Business & Management'),
+(8,18,'Health Awareness Fair','A fair to promote health awareness and provide free health checkups.','Community Health Network','9101 Health Blvd, Houston, TX','2024-09-18','2024-09-18','09:00:00','14:00:00',5,'Medium',20,'Legal & Advocacy'),
+(9,19,'Urban Planning Forum','Discussing urban planning initiatives for the future of Houston.','Urban Development Group','7890 Urban St, Houston, TX','2024-09-22','2024-09-22','10:00:00','12:00:00',2,'Low',15,'Transportation'),
+(10,20,'Education Seminar','A seminar focused on the importance of education for all.','Education for All Organization','3456 Education Ln, Houston, TX','2024-09-30','2024-09-30','11:00:00','15:00:00',4,'High',20,'Performing Arts');
+/*!40000 ALTER TABLE `EventDetails` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -125,7 +134,7 @@ CREATE TABLE `EventSkills` (
   `skillId` int NOT NULL,
   PRIMARY KEY (`eventId`,`skillId`),
   KEY `skillId` (`skillId`),
-  CONSTRAINT `eventskills_fkey_1` FOREIGN KEY (`eventId`) REFERENCES `Event_details` (`eventId`),
+  CONSTRAINT `eventskills_fkey_1` FOREIGN KEY (`eventId`) REFERENCES `EventDetails` (`eventId`),
   CONSTRAINT `eventskills_ibfk_2` FOREIGN KEY (`skillId`) REFERENCES `Skills` (`skillId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -221,18 +230,37 @@ CREATE TABLE `UserProfile` (
 
 LOCK TABLES `UserProfile` WRITE;
 /*!40000 ALTER TABLE `UserProfile` DISABLE KEYS */;
-INSERT INTO `UserProfile` VALUES (10000,'user10000@example.com','Alice Jonhnson','admin','123 Elm St', '','Houston','Texas',62701,'2024-07-01','','',''),(10001,'user10001@example.com','Keanu Reeves','volunteer','456 Oak St','','Katy','Texas',62701,'2024-07-02','','',''),(10002,'user10002@example.com','Scarlett Johanson','volunteer','789 Pine St','','Pasedena','Texas',62701,'2024-07-03','','',''),(10003,'user10003@example.com','Ivy Cooper','admin','101 Maple St','','Spring','Texas',62701,'2024-07-04','','',''),(10004,'user10004@example.com','Bobby Singer','volunteer','202 Birch St','','Woodlands','Texas',62701,'2024-07-05','','',''),(10005,'user10005@example.com','Emily Jackson','admin','303 Cedar St','','Spring','Texas',62701,'2024-07-06','','',''),(10006,'user10006@example.com','Olivia Harris','volunteer','404 Fir St','','Cypress','Texas',62701,'2024-07-07','','',''),(10007,'user10007@example.com','Maryanne Scout','volunteer','505 Spruce St','','Sugar Land','Texas',62701,'2024-07-08','','',''),(10008,'user10008@example.com','Margaret Thatcher','admin','606 Willow St','','Conroe','Texas',62701,'2024-07-09','','',''),(10009,'user10009@example.com','Jessica Cliff','volunteer','707 Ash St','','Cinco Ranch','Texas',62701,'2024-07-10','','',''),(10010,'user10010@example.com','Marianna Lee','volunteer','808 Pine St','','Richmond','Texas',62701,'2024-07-11','','',''),(10011,'user10011@example.com','Dallas Winston','admin','909 Oak St','','Humble','Texas',62701,'2024-07-12','','',''),(10012,'user10012@example.com','Austin Miller','volunteer','1010 Elm St','','Channelview','Texas',62701,'2024-07-13','','',''),(10013,'user10013@example.com','Sunny King','admin','1111 Maple St','','Aldine','Texas',62701,'2024-07-14','','',''),(10014,'user10014@example.com','Darris Rogers','volunteer','1212 Cedar St','','Missouri City','Texas',62701,'2024-07-15','','','');
+INSERT INTO UserProfile (userId, email, fullName, skills, address1, address2, city, state, zipcode, regDate, preferences, availability, userRole) VALUES
+(1,'jdoe@example.com','John Doe','Computer & IT','123 Main St','','Houston','Texas',77001,'2024-08-01','', 'Monday 7:00am to 9:00am', 'Admin'),
+(2,'asmith@example.com','Alice Smith','Education & Literacy','456 Oak St','','Sugar Land','Texas',77479,'2024-08-02','', 'Tuesday 12:00pm to 4:00pm', 'Admin'),
+(3,'eclark@example.com','Eva Clark','Healthcare & Social Services','987 Cedar St','','Cypress','Texas',77433,'2024-08-03','', 'Wednesday 10:00am to 12:00pm', 'Admin'),
+(4,'kmorgan@example.com','Kelly Morgan','Business & Management','753 Dogwood St','','Missouri City','Texas',77489,'2024-08-04','', 'Thursday 1:00pm to 5:00pm', 'Admin'),
+(5,'jones@example.com','Jordan Jones','Marketing & Communications','852 Redwood St','','Pasadena','Texas',77506,'2024-08-05','', 'Friday 9:00am to 11:00am', 'Admin'),
+(6,'bjohnson@example.com','Bob Johnson','Interactive & Web Development','789 Pine St','','Spring','Texas',77373,'2024-08-06','', 'Monday 2:00pm to 6:00pm', 'Volunteer'),
+(7,'dwilson@example.com','David Wilson','Hobbies & Crafts','654 Maple St','','Katy','Texas',77494,'2024-08-07','', 'Tuesday 8:00am to 10:00am', 'Volunteer'),
+(8,'fthomas@example.com','Frank Thomas','Performing Arts','159 Birch St','','Tomball','Texas',77375,'2024-08-08','', 'Wednesday 3:00pm to 7:00pm', 'Volunteer'),
+(9,'hharris@example.com','Henry Harris','Sports & Recreation','951 Ash St','','Houston','Texas',77002,'2024-08-09','', 'Thursday 9:00am to 11:00am', 'Volunteer'),
+(10,'ijones@example.com','Ivy Jones','Language & Culture','852 Redwood St','','Sugar Land','Texas',77478,'2024-08-10','', 'Friday 12:00pm to 2:00pm', 'Volunteer'),
+(11,'jmartin@example.com','Jack Martin','Disaster Relief','654 Spruce St','','Spring','Texas',77379,'2024-08-11','', 'Monday 10:00am to 12:00pm', 'Volunteer'),
+(12,'lreyes@example.com','Leo Reyes','Arts','159 Palm St','','Katy','Texas',77450,'2024-08-12','', 'Tuesday 3:00pm to 5:00pm', 'Volunteer'),
+(13,'mlopez@example.com','Mona Lopez','Transportation','321 Poplar St','','Cypress','Texas',77429,'2024-08-13','', 'Wednesday 9:00am to 11:00am', 'Volunteer'),
+(14,'nking@example.com','Nina King','Legal & Advocacy','456 Cherry St','','Tomball','Texas',77377,'2024-08-14','', 'Thursday 2:00pm to 4:00pm', 'Volunteer'),
+(15,'omartin@example.com','Olivia Martin','Trades','753 Willow St','','Pasadena','Texas',77505,'2024-08-15','', 'Friday 7:00am to 9:00am', 'Volunteer'),
+(16,'james.smith@example.com','James Smith','HR','123 Maple St','','Sugar Land','Texas',77479,'2024-08-16','', 'Monday 8:00am to 10:00am', 'Admin'),
+(17,'lisa.johnson@example.com','Lisa Johnson','Finance','456 Oak Ave','','Spring','Texas',77386,'2024-08-17','', 'Tuesday 1:00pm to 3:00pm', 'Admin'),
+(18,'michael.brown@example.com','Michael Brown','Food Service & Events','789 Pine Rd','','Houston','Texas',77001,'2024-08-18','', 'Wednesday 11:00am to 1:00pm', 'Admin'),
+(19,'emily.wilson@example.com','Emily Wilson','Academics','101 Birch Blvd','','Katy','Texas',77450,'2024-08-19','', 'Thursday 4:00pm to 6:00pm', 'Admin');
 /*!40000 ALTER TABLE `UserProfile` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Vol_History`
+-- Table structure for table `VolunteerEvents`
 --
 
-DROP TABLE IF EXISTS `Vol_History`;
+DROP TABLE IF EXISTS `VolunteerEvents`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Vol_History` (
+CREATE TABLE `VolunteerEvents` (
   `eventId` int NOT NULL,
   `userId` int NOT NULL,
   `hoursWorked` float DEFAULT NULL,
@@ -240,19 +268,19 @@ CREATE TABLE `Vol_History` (
   `perfomScore` float DEFAULT NULL,
   PRIMARY KEY (`eventId`,`userId`),
   KEY `userId` (`userId`),
-  CONSTRAINT `vol_history_fkey_1` FOREIGN KEY (`eventId`) REFERENCES `Event_details` (`eventId`),
-  CONSTRAINT `vol_history_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `Volunteers` (`userId`)
+  CONSTRAINT `Volunteer_Events_fkey_1` FOREIGN KEY (`eventId`) REFERENCES `EventDetails` (`eventId`),
+  CONSTRAINT `Volunteer_Events_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `Volunteers` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Vol_History`
+-- Dumping data for table `VolunteerEvents`
 --
 
-LOCK TABLES `Vol_History` WRITE;
-/*!40000 ALTER TABLE `Vol_History` DISABLE KEYS */;
-INSERT INTO `Vol_History` VALUES (11,10005,2,NULL,NULL),(12,10005,1.5,NULL,NULL),(12,10007,1.5,NULL,NULL),(25,10006,2,NULL,NULL),(25,10014,2,NULL,NULL);
-/*!40000 ALTER TABLE `Vol_History` ENABLE KEYS */;
+LOCK TABLES `VolunteerEvents` WRITE;
+/*!40000 ALTER TABLE `VolunteerEvents` DISABLE KEYS */;
+INSERT INTO `VolunteerEvents` VALUES (11,10005,2,NULL,NULL),(12,10005,1.5,NULL,NULL),(12,10007,1.5,NULL,NULL),(25,10006,2,NULL,NULL),(25,10014,2,NULL,NULL);
+/*!40000 ALTER TABLE `VolunteerEvents` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
